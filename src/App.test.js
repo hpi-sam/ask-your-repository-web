eef30e11160123b92e7d09f6155dbfa1c1815568
Erik Ziegler <erik.ziegler@student.hpi.uni-potsdam.de>
2018-11-05 17:13:21 +0100

    Setup enzyme for testing React components

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('<App />', () => {
+  it('renders correctly', () => {
+    const wrapper = shallow(<App />);
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
 });
