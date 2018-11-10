6a753500ed9bffaff4e802c1ad87f3c445f11fb6
Erik Ziegler <erik.ziegler@student.hpi.uni-potsdam.de>
2018-11-10 17:26:05 +0100

    Disable redux-logger middleware for production build

--- src/Root.js
+++ src/Root.js
@@ -2,11 +2,17 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
-import logger from 'redux-logger';
 import rootReducer from './state/rootReducer';
 import App from './App';
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+let middleware = [];
+
+if (process.env.NODE_ENV !== 'production') {
+  const logger = require('redux-logger').default;
+  middleware = [...middleware, logger];
+}
+
+const store = createStore(rootReducer, applyMiddleware(...middleware));
 
 function Root() {
   return (
