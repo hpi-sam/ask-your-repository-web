efd961b1ebd71dfb4315c90d4c232d1fe40427c8
luisebenkert <luise.benkert@student.hpi.uni-potsdam.de>
2018-11-20 17:15:53 +0100

    Add routing and image grid gallery. Ref #6

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style/index.scss';
 import Root from './Root';
 import * as serviceWorker from './serviceWorker';
+import './style/index.scss';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+ReactDOM.render(
+  <Root />,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
