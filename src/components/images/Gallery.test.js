d5545a2aed916dd2b404058958f897a696e64284
luise.benkert <luise.benkert@student.hpi.uni-potsdam.de>
2018-11-29 10:56:09 +0100

    Fix error when image tags are not array. Ref #6

--- src/components/images/Gallery.test.js
+++ src/components/images/Gallery.test.js
@@ -1,31 +1,27 @@
 // @flow
 import _ from 'lodash';
 import React from 'react';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import Gallery from './Gallery';
 import ImageFactory from '../../factories/ImageFactory';
 
 describe('<Gallery />', () => {
-  const tagAmount = 6;
-  const images = _.times(2, () => ImageFactory.createDummyImage(tagAmount));
-  let wrapper;
-
-  beforeEach(() => {
-    wrapper = mount(<Gallery images={images} />);
-  });
-
-  it('renders', () => {
-    expect(wrapper.exists()).toBeTruthy();
-  });
-
   it('should only render the first 5 tags', () => {
-    const image = images[0];
-    const text = wrapper.text();
+    const images = _.times(2, () => ImageFactory.createDummyImage(6));
+    const text = mount(<Gallery images={images} />).text();
 
     _.times(5, (i) => {
-      expect(text).toEqual(expect.stringContaining(image.tags[i]));
+      expect(text).toEqual(expect.stringContaining(images[0].tags[i]));
     });
 
-    expect(text).toEqual(expect.not.stringContaining(image.tags[5]));
+    expect(text).toEqual(expect.not.stringContaining(images[0].tags[5]));
+  });
+
+  it('should catch missing tags property', () => {
+    const taglessImage: any = ImageFactory.createDummyImage(0);
+    taglessImage.tags = null;
+    const wrapper = shallow(<Gallery images={[taglessImage]} />);
+
+    expect(wrapper.exists()).toBeTruthy();
   });
 });
