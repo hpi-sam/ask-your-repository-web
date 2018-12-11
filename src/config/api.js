950578778895ef267592b12c7fe32f1e85c692a4
Jascha Beste <bestejascha@gmail.com>
2018-12-11 09:34:19 +0100

    Fix params serializer

--- src/config/api.js
+++ src/config/api.js
@@ -5,8 +5,8 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   requestInterceptor: config => ({
     ...config,
-    paramsSerializer: params => qs.stringify(params, { arrayFormat: 'repeat' }),
   }),
+  paramsSerializer: params => qs.stringify(params, { arrayFormat: 'repeat' }),
 });
 
 export default api;
