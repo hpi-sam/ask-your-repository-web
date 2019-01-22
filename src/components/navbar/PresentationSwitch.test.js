51892446d8de016d2e6458bf8b531eb414113fe0
Adrian Steppat <adrian.steppat@web.de>
2019-01-22 10:25:01 +0100

    Add tests for PresenationSwitch

--- src/components/navbar/PresentationSwitch.test.js
+++ src/components/navbar/PresentationSwitch.test.js
@@ -5,6 +5,7 @@ import { shallow } from 'enzyme';
 import configureStore from 'redux-mock-store';
 import initialState from '../../state/initialState';
 import PresentationSwitch from './PresentationSwitch';
+import { turnOnPresentationMode, turnOffPresentationMode } from '../../state/presentation_mode/presentationMode.actionCreators';
 
 const mockStore = configureStore();
 
@@ -21,5 +22,21 @@ describe('<PresentationSwitch />', () => {
 
   it('renders correctly', () => {
     expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('turns on PresentationMode on click', () => {
+    wrapper.find('button').simulate('click');
+    expect(store.getActions()).toEqual([turnOnPresentationMode()]);
+  });
+
+  it('turns off presentation mode if it was on before', () => {
+    const storeState = initialState;
+    storeState.presentationMode.isActive = true;
+    store = mockStore(storeState);
+    wrapper = shallow((
+      <PresentationSwitch store={store} />
+    )).dive();
+    wrapper.find('button').simulate('click');
+    expect(store.getActions()).toEqual([turnOffPresentationMode()]);
   });
 });
