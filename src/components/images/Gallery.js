6c6e5daec5ef8a3fa0384b877f6f2a044ab22860
arne-z <arne.zerndt@student.hpi.de>
2019-01-31 22:17:20 +0100

    Add image detail and edit page.

--- src/components/images/Gallery.js
+++ src/components/images/Gallery.js
@@ -1,58 +1,23 @@
 // @flow
-import React, { Component } from 'react';
-import { IoIosMore } from 'react-icons/io';
-import shortid from 'shortid';
-import Tag from '../utility/Tag';
+import React from 'react';
+import GalleryItem from './GalleryItem';
 import type { Image } from '../../models/Image';
 import './Gallery.scss';
-
-const maxTags = 5;
 
 type Props = {
   images: Array<Image>,
 };
 
-class Gallery extends Component<Props> {
-  renderImage = (image: Image) => {
-    const { url } = image;
+function Gallery(props: Props) {
+  const { images } = props;
 
-    const tags = image.tags || [];
-    const displayedTags = tags.slice(0, maxTags);
-    const showEllipses = tags.length >= maxTags;
-
-    return (
-      <div key={image.id} className="Gallery__item">
-        <img
-          className="Gallery__item__image"
-          src={url}
-          alt={displayedTags.join(', ')}
-        />
-        <div className="Gallery__item__overlay">
-          {displayedTags.map(tag => (
-            <Tag
-              key={shortid.generate()}
-              caption={tag}
-            />
-          ))}
-          {showEllipses && (
-            <div className="Gallery__item__ellipses">
-              <IoIosMore />
-            </div>
-          )}
-        </div>
-      </div>
-    );
-  };
-
-  render() {
-    const { images } = this.props;
-
-    return (
-      <div className="Gallery">
-        {images.map(this.renderImage)}
-      </div>
-    );
-  }
+  return (
+    <div className="Gallery">
+      {images.map((image: Image) => (
+        <GalleryItem key={image.id} image={image} />
+      ))}
+    </div>
+  );
 }
 
 export default Gallery;
