938b2aea3b9d5fc9312aeac3502636f31cebc0af
Erik Ziegler <erik.ziegler@student.hpi.uni-potsdam.de>
2019-02-18 21:18:56 +0100

    Add e2e tests for image edit.

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -17,6 +17,38 @@ Cypress.Commands.add('setActiveTeam', () => {
     });
 });
 
+Cypress.Commands.add('createImage', (fixture, teamId, tags = []) => {
+  const url = `${Cypress.env('API_URL')}/images`;
+
+  cy.server();
+  cy.route({ method: 'POST', url }).as('createImage');
+
+  const sendRequest = (formData) => {
+    cy.window().then((window) => {
+      const xhr = new window.XMLHttpRequest();
+      xhr.open('POST', url);
+      xhr.send(formData);
+    });
+  };
+
+  return cy.fixture(fixture, 'base64')
+    .then(Cypress.Blob.base64StringToBlob)
+    .then((blob) => {
+      cy.window().then((window) => {
+        const file = new window.File([blob], fixture);
+        const formData = new FormData();
+        formData.append('image', file);
+        formData.append('team_id', teamId);
+        formData.append('tags', tags);
+
+        sendRequest(formData);
+
+        return cy.wait('@createImage')
+          .then(xhr => xhr.response.body);
+      });
+    });
+});
+
 Cypress.Commands.add('drop', {
   prevSubject: 'element',
 }, (subject, fileUrls) => {
