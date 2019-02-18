874c7a8a694a50533f1f1fe928fd0238b03c3dfe
Erik Ziegler <erik.ziegler@student.hpi.uni-potsdam.de>
2019-02-18 21:18:56 +0100

    Add e2e tests for image edit.

---
+++ cypress/integration/edit_image.js
@@ -0,0 +1,57 @@
+context('Edit Image', () => {
+  let image;
+
+  beforeEach(() => {
+    cy.server();
+
+    cy.setActiveTeam()
+      .then((team) => {
+        cy.createImage('goat.jpg', team.id, ['Goat'])
+          .then((createdImage) => {
+            image = createdImage;
+            cy.visit(`/images/${image.id}/edit`);
+            cy.route({ method: 'PATCH', url: `/images/${image.id}` }).as('updateImage');
+          });
+      });
+  });
+
+  it('redirects to the detail page on successful submit', () => {
+    cy.get('[data-cy=save-button]').click();
+    cy.wait('@updateImage');
+    cy.location().should((location) => {
+      expect(location.pathname).to.eq(`/images/${image.id}`);
+    });
+  });
+
+  it('saves added tags', () => {
+    cy.get('[data-cy=tag-selector-input]')
+      .type('Nice{enter}')
+      .type('Cool{enter}');
+
+    cy.get('[data-cy=save-button]').click();
+    cy.wait('@updateImage');
+
+    const expectedTags = [...image.tags, 'Nice', 'Cool'];
+
+    cy.request(`${Cypress.env('API_URL')}/images/${image.id}`)
+      .its('body.tags')
+      .should('have.members', expectedTags);
+  });
+
+  /*
+  FIX ME: Currently failing because of missing backend functionality
+  it('saves removed tags', () => {
+    cy.get('[data-cy=tag-selector-input]')
+      .type('{backspace}');
+
+    cy.get('[data-cy=save-button]').click();
+    cy.wait('@updateImage');
+
+    const expectedTags = image.tags.slice(0, -1);
+
+    cy.request(`${Cypress.env('API_URL')}/images/${image.id}`)
+      .its('body.tags')
+      .should('have.members', expectedTags);
+  });
+  */
+});
