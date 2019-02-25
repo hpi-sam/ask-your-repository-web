15017cf372d8161778e9ed06ef0a8fe34656543e
Adrian Steppat <adrian.steppat@web.de>
2019-02-25 16:11:30 +0100

    Change route for authentication API calls.

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -40,7 +40,7 @@ class UserService {
 
   static async login(emailOrUsername: string, password: string): Promise<User> {
     try {
-      const response = await api.post('/users/login', humps.decamelizeKeys({ emailOrUsername, password }), { withCredentials: true });
+      const response = await api.post('/authentications', humps.decamelizeKeys({ emailOrUsername, password }), { withCredentials: true });
       localStorage.setItem('user', JSON.stringify(response.data));
       return response.data;
     } catch (e) {
@@ -51,7 +51,7 @@ class UserService {
   static async logout() {
     try {
       const headers = authHeader();
-      await api.post('/users/logout', {}, { headers, withCredentials: true });
+      await api.delete('/authentications', {}, { headers, withCredentials: true });
     } finally {
       localStorage.removeItem('user');
     }
