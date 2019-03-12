9adc406e5a6468b2c13ed0f7716f4daa753fada1
Erik Ziegler <erik.ziegler@student.hpi.uni-potsdam.de>
2019-03-12 14:54:55 +0100

    Remove tag suggestions shortcut.

--- src/components/tagging/form/TagSuggestions.js
+++ src/components/tagging/form/TagSuggestions.js
@@ -1,5 +1,5 @@
 // @flow
-import React, { useEffect } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import shortid from 'shortid';
 import TagSuggestion from './TagSuggestion';
@@ -18,30 +18,12 @@ type Props = {
 function TagSuggestions(props: Props) {
   const suggestions = useTagSuggestions(props.activeTeam, props.tags);
 
-  const handleKeyPress = (event: SyntheticKeyboardEvent<*>) => {
-    const index = parseInt(event.key, 10) - 1;
-    const suggestion = suggestions[index];
-    if (!suggestion) return;
-
-    event.preventDefault();
-    props.onSuggestionClick(suggestion);
-  };
-
-  useEffect(() => {
-    window.addEventListener('keypress', handleKeyPress);
-
-    return () => {
-      window.removeEventListener('keypress', handleKeyPress);
-    };
-  });
-
   return (
     <div className="TagSuggestions">
-      {suggestions.map((tag, index) => (
+      {suggestions.map(tag => (
         <TagSuggestion
           onClick={props.onSuggestionClick}
           tag={tag}
-          shortcut={(index + 1).toString()}
           key={shortid.generate()}
         />
       ))}
