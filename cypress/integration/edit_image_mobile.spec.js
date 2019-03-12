04cdea40aa66a2ad4939333153622096768fb8e0
Erik Ziegler <erik.ziegler@student.hpi.uni-potsdam.de>
2019-03-12 14:52:02 +0100

    Reduce cypress tests for mobile tagging.

--- cypress/integration/edit_image_mobile.spec.js
+++ cypress/integration/edit_image_mobile.spec.js
@@ -17,15 +17,8 @@ context('Edit Image Mobile', () => {
       });
   });
 
-  it('redirects to the detail page on successful submit', () => {
-    cy.get('[data-cy=save-button]').click();
-    cy.wait('@updateImage');
-    cy.location().should((location) => {
-      expect(location.pathname).to.eq(`/images/${image.id}`);
-    });
-  });
-
-  it('saves added tags', () => {
+  it('saves the tags', () => {
+    cy.get('[data-cy=tag-remove-button-Goat]').click();
     cy.get('[data-cy=tag-input]')
       .type('Nice{enter}')
       .type('Cool{enter}');
@@ -33,20 +26,7 @@ context('Edit Image Mobile', () => {
     cy.get('[data-cy=save-button]').click();
     cy.wait('@updateImage');
 
-    const expectedTags = [...image.userTags, 'Nice', 'Cool'];
-
-    cy.request(`${Cypress.env('API_URL')}/images/${image.id}`)
-      .its('body.user_tags')
-      .should('have.members', expectedTags);
-  });
-
-  it('saves removed tags', () => {
-    cy.get('[data-cy=tag-remove-button-Goat]').click();
-    cy.get('[data-cy=tag-input]').type('Sheep{enter}');
-    cy.get('[data-cy=save-button]').click();
-    cy.wait('@updateImage');
-
-    const expectedTags = [...image.userTags.slice(0, -1), 'Sheep'];
+    const expectedTags = ['Nice', 'Cool'];
 
     cy.request(`${Cypress.env('API_URL')}/images/${image.id}`)
       .its('body.user_tags')
