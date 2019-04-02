eb58b3610ad16f9daf5fa93a6e5c075e725cde5d
Adrian Steppat <adrian.steppat@student.hpi.de>
2019-04-02 18:56:11 +0200

    change google api init

--- src/config/createGoogleAuthInstance.js
+++ src/config/createGoogleAuthInstance.js
@@ -1,16 +1,14 @@
 // @flow
 
-function createGoogleAuthInstance(): Promise<any> {
-  return new Promise((resolve) => {
-    require('google-client-api')().then((gapi) => {
-      gapi.client.init({
-        clientId: process.env.REACT_APP_GOOGLE_CLIENT_ID,
-        scope: 'profile email',
-      }).then(() => {
-        resolve(gapi.auth2.getAuthInstance());
-      });
+async function createGoogleAuthInstance() {
+  const gapi = await require('google-client-api')();
+  if (!gapi.auth2.getAuthInstance()) {
+    await gapi.auth2.init({
+      clientId: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+      scope: 'profile email',
     });
-  });
+  }
+  return gapi.auth2.getAuthInstance();
 }
 
 export default createGoogleAuthInstance;
