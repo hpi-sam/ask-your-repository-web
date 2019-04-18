77ec5d8adf50132fd6e4b7fc03629f69738f3488
Adrian Steppat <adrian.steppat@student.hpi.de>
2019-04-18 11:44:19 +0200

    Add scroll to top in Unauthorized App

--- src/components/UnauthorizedApp.js
+++ src/components/UnauthorizedApp.js
@@ -1,6 +1,7 @@
 // @flow
 import React, { Fragment } from 'react';
 import { Route, Switch } from 'react-router';
+import type { Location } from 'react-router';
 import LoginForm from './auth/LoginForm';
 import RegisterForm from './auth/RegisterForm';
 import PasswordRequestForm from './auth/forgot_password/PasswordRequestForm';
@@ -11,22 +12,34 @@ import AuthorizedApp from './AuthorizedApp';
 import AuthenticatedRoute from './custom_routes/AuthenticatedRoute';
 import './UnauthorizedApp.scss';
 
-function UnauthorizedApp() {
-  return (
-    <Fragment>
-      <div className="UnauthorizedApp">
-        <Switch>
-          <Route path="/login" component={LoginForm} />
-          <Route path="/register" component={RegisterForm} />
-          <Route path="/forgot-password" component={PasswordRequestForm} />
-          <Route path="/reset-password" component={ResetPasswordForm} />
-          <Route exact path="/" component={LandingPage} />
-          <AuthenticatedRoute path="*" component={AuthorizedApp} />
-        </Switch>
-      </div>
-      <UnauthorizedFooter />
-    </Fragment>
-  );
+type Props = {
+  location: Location,
+};
+
+class UnauthorizedApp extends React.Component<Props> {
+  componentDidUpdate(prevProps: Props) {
+    if (this.props.location.pathname !== prevProps.location.pathname) {
+      window.scrollTo(0, 0);
+    }
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div className="UnauthorizedApp">
+          <Switch>
+            <Route path="/login" component={LoginForm} />
+            <Route path="/register" component={RegisterForm} />
+            <Route path="/forgot-password" component={PasswordRequestForm} />
+            <Route path="/reset-password" component={ResetPasswordForm} />
+            <Route exact path="/" component={LandingPage} />
+            <AuthenticatedRoute path="*" component={AuthorizedApp} />
+          </Switch>
+        </div>
+        <UnauthorizedFooter />
+      </Fragment>
+    );
+  }
 }
 
 export default UnauthorizedApp;
