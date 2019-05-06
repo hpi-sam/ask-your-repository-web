f3db57f11f45173da8911967647acb119e70b00d
Jascha Beste <bestejascha@gmail.com>
2019-05-06 16:18:53 +0200

    Fix cypress invitation link test.

--- cypress/integration/team_invitation_link.spec.js
+++ cypress/integration/team_invitation_link.spec.js
@@ -2,18 +2,20 @@ context('Team Invitation Link', () => {
   let team;
 
   beforeEach(() => {
+    cy.server();
     cy.resetDB();
     cy.authenticate();
     cy.setActiveTeam()
       .then((activeTeam) => {
         team = activeTeam;
+        cy.route({ method: 'GET', url: `/teams/${team.id}` }).as('team');
       });
   });
 
   it('adds current member to team after following invitation link', () => {
     cy.get('[data-cy=navbar-team-button]').click();
     cy.get('[data-cy=team-sidebar-settings-dropdown-button]').click();
-    cy.get('[data-cy=team-sidebar-settings-invite-button]').click();
+    cy.wait('@team');
     cy.get('[data-cy=team-sidebar-settings-invite-input]')
       .invoke('val')
       .then((url) => {
